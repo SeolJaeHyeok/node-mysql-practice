ecd1af6369622af216839ed0307bd526e5c7272d
Jaehyeok <milkboy2564@naver.com>
2022-05-16 02:28:27 +0900

    Add features

--- main.js
+++ main.js
@@ -35,50 +35,70 @@ const renderRootPage = (response, err, topics) => {
 const renderDetailPage = (response, err, topics, topicId) => {
   if (err) throw err;
 
-  db.query(`SELECT * FROM topic WHERE id=?`, [topicId], (err1, topic) => {
-    if (err1) throw err1;
-    var title = topic[0].title;
-    var description = topic[0].description;
-    var list = template.list(topics);
-    var html = template.HTML(
-      title,
-      list,
-      `<h2>${title}</h2>${description}`,
-      ` <a href="/create">create</a>
+  db.query(
+    `SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`,
+    [topicId],
+    (err1, topic) => {
+      if (err1) throw err1;
+      console.log(topic);
+      var title = topic[0].title;
+      var description = topic[0].description;
+      var list = template.list(topics);
+      var html = template.HTML(
+        title,
+        list,
+        `
+          <h2>
+            ${title}
+          </h2>
+          ${description}
+          <h4>
+            Written By : ${topic[0].name}, ${topic[0].profile}
+
+          </h4>
+        `,
+        ` <a href="/create">create</a>
         <a href="/update?id=${topicId}">update</a>
           <form action="delete_process" method="post">
             <input type="hidden" name="id" value="${topicId}">
             <input type="submit" value="delete">
           </form>`
-    );
-    response.writeHead(200);
-    response.end(html);
-  });
+      );
+      response.writeHead(200);
+      response.end(html);
+    }
+  );
 };
 
 const renderCreatePage = (response, err, topics) => {
   if (err) throw err;
+  db.query(`SELECT * FROM author`, (err1, authors) => {
+    if (err1) throw err1;
 
-  var title = "Create";
-  var list = template.list(topics);
-  var html = template.HTML(
-    title,
-    list,
-    `
+    var title = "Create";
+    var list = template.list(topics);
+    var html = template.HTML(
+      title,
+      list,
+      `
     <form action="/create_process" method="post">
       <p><input type="text" name="title" placeholder="title"></p>
       <p>
         <textarea name="description" placeholder="description"></textarea>
       </p>
       <p>
+        ${template.selectAuthor(authors)}
+      </p>
+      <p>
         <input type="submit">
       </p>
     </form>
     `,
-    `<a href="/create">create</a>`
-  );
-  response.writeHead(200);
-  response.end(html);
+      `<a href="/create">create</a>`
+    );
+    response.writeHead(200);
+    response.end(html);
+  });
 };
 
 const renderCreateProcessPage = (request, response) => {
@@ -90,7 +110,7 @@ const renderCreateProcessPage = (request, response) => {
     var post = qs.parse(body);
     db.query(
       "INSERT INTO topic (title, description, created, author_id) VALUES (? , ?, NOW(), ?)",
-      [post.title, post.description, 1],
+      [post.title, post.description, post.author],
       (err, result) => {
         if (err) throw err;
 
@@ -106,27 +126,35 @@ const renderUpdatePage = (response, err, topics, topicId) => {
 
   db.query(`SELECT * FROM topic WHERE id=?`, [topicId], (err1, topic) => {
     if (err1) throw err1;
-
-    var list = template.list(topics);
-    var html = template.HTML(
-      topic[0].title,
-      list,
-      `
-        <form action="/update_process" method="post">
-          <input type="hidden" name="id" value="${topic[0].id}">
-          <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${topic[0].description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-      `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
-    );
-    response.writeHead(200);
-    response.end(html);
+    db.query(`SELECT * FROM author`, (err2, authors) => {
+      var list = template.list(topics);
+      var html = template.HTML(
+        topic[0].title,
+        list,
+        `
+          <form action="/update_process" method="post">
+            <input type="hidden" name="id" value="${topic[0].id}">
+            <p><input type="text" name="title" placeholder="title" value="${
+              topic[0].title
+            }"></p>
+            <p>
+              <textarea name="description" placeholder="description">${
+                topic[0].description
+              }</textarea>
+            </p>
+            <p>
+              ${template.selectAuthor(authors, topic[0].author_id)}
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+          `,
+        `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
+      );
+      response.writeHead(200);
+      response.end(html);
+    });
   });
 };
 
@@ -139,7 +167,7 @@ const renderUpdateProcessPage = (request, response) => {
     var post = qs.parse(body);
     db.query(
       `UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`,
-      [post.title, post.description, 1, post.id],
+      [post.title, post.description, post.author, post.id],
       (err, result) => {
         if (err) throw err;
         response.writeHead(302, { Location: `/?id=${post.id}` });
