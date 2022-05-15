2059ef63477e8f0a5eb286832098514f8c9f59b2
Jaehyeok <milkboy2564@naver.com>
2022-05-16 01:49:06 +0900

    CRUD

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const db = mysql.createConnection({
 
 db.connect();
 
-const rootPageRendering = (response, err, topics) => {
+const renderRootPage = (response, err, topics) => {
   if (err) throw err;
 
   var title = "Welcome";
@@ -32,7 +32,7 @@ const rootPageRendering = (response, err, topics) => {
   response.end(html);
 };
 
-const detailPageRendering = (response, err, topics, topicId) => {
+const renderDetailPage = (response, err, topics, topicId) => {
   if (err) throw err;
 
   db.query(`SELECT * FROM topic WHERE id=?`, [topicId], (err1, topic) => {
@@ -56,6 +56,114 @@ const detailPageRendering = (response, err, topics, topicId) => {
   });
 };
 
+const renderCreatePage = (response, err, topics) => {
+  if (err) throw err;
+
+  var title = "Create";
+  var list = template.list(topics);
+  var html = template.HTML(
+    title,
+    list,
+    `
+    <form action="/create_process" method="post">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p>
+        <textarea name="description" placeholder="description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
+    `,
+    `<a href="/create">create</a>`
+  );
+  response.writeHead(200);
+  response.end(html);
+};
+
+const renderCreateProcessPage = (request, response) => {
+  var body = "";
+  request.on("data", function (data) {
+    body = body + data;
+  });
+  request.on("end", function () {
+    var post = qs.parse(body);
+    db.query(
+      "INSERT INTO topic (title, description, created, author_id) VALUES (? , ?, NOW(), ?)",
+      [post.title, post.description, 1],
+      (err, result) => {
+        if (err) throw err;
+
+        response.writeHead(302, { Location: `/?id=${result.insertId}` });
+        response.end();
+      }
+    );
+  });
+};
+
+const renderUpdatePage = (response, err, topics, topicId) => {
+  if (err) throw err;
+
+  db.query(`SELECT * FROM topic WHERE id=?`, [topicId], (err1, topic) => {
+    if (err1) throw err1;
+
+    var list = template.list(topics);
+    var html = template.HTML(
+      topic[0].title,
+      list,
+      `
+        <form action="/update_process" method="post">
+          <input type="hidden" name="id" value="${topic[0].id}">
+          <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
+          <p>
+            <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        `,
+      `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
+    );
+    response.writeHead(200);
+    response.end(html);
+  });
+};
+
+const renderUpdateProcessPage = (request, response) => {
+  var body = "";
+  request.on("data", function (data) {
+    body = body + data;
+  });
+  request.on("end", function () {
+    var post = qs.parse(body);
+    db.query(
+      `UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`,
+      [post.title, post.description, 1, post.id],
+      (err, result) => {
+        if (err) throw err;
+        response.writeHead(302, { Location: `/?id=${post.id}` });
+        response.end();
+      }
+    );
+  });
+};
+
+const renderDeleteProcessPage = (request, response) => {
+  var body = "";
+  request.on("data", function (data) {
+    body = body + data;
+  });
+  request.on("end", function () {
+    var post = qs.parse(body);
+    db.query(`DELETE FROM topic WHERE id = ?`, [post.id], (err, result) => {
+      if (err) throw err;
+      response.writeHead(302, { Location: `/` });
+      response.end();
+    });
+  });
+};
+
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
@@ -63,108 +171,27 @@ var app = http.createServer(function (request, response) {
   if (pathname === "/") {
     if (queryData.id === undefined) {
       db.query(`SELECT * FROM topic`, (err, topics) =>
-        rootPageRendering(response, err, topics)
+        renderRootPage(response, err, topics)
       );
     } else {
       db.query(`SELECT * FROM topic`, (err, topics) => {
-        detailPageRendering(response, err, topics, queryData.id);
+        renderDetailPage(response, err, topics, queryData.id);
       });
     }
   } else if (pathname === "/create") {
-    fs.readdir("./data", function (error, filelist) {
-      var title = "WEB - create";
-      var list = template.list(filelist);
-      var html = template.HTML(
-        title,
-        list,
-        `
-          <form action="/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-        `,
-        ""
-      );
-      response.writeHead(200);
-      response.end(html);
-    });
+    db.query(`SELECT * FROM topic`, (err, topics) =>
+      renderCreatePage(response, err, topics)
+    );
   } else if (pathname === "/create_process") {
-    var body = "";
-    request.on("data", function (data) {
-      body = body + data;
-    });
-    request.on("end", function () {
-      var post = qs.parse(body);
-      var title = post.title;
-      var description = post.description;
-      fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-        response.writeHead(302, { Location: `/?id=${title}` });
-        response.end();
-      });
-    });
+    renderCreateProcessPage(request, response);
   } else if (pathname === "/update") {
-    fs.readdir("./data", function (error, filelist) {
-      var filteredId = path.parse(queryData.id).base;
-      fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-        var title = queryData.id;
-        var list = template.list(filelist);
-        var html = template.HTML(
-          title,
-          list,
-          `
-            <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
-      });
-    });
+    db.query(`SELECT * FROM topic`, (err, topics) =>
+      renderUpdatePage(response, err, topics, queryData.id)
+    );
   } else if (pathname === "/update_process") {
-    var body = "";
-    request.on("data", function (data) {
-      body = body + data;
-    });
-    request.on("end", function () {
-      var post = qs.parse(body);
-      var id = post.id;
-      var title = post.title;
-      var description = post.description;
-      fs.rename(`data/${id}`, `data/${title}`, function (error) {
-        fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-          response.writeHead(302, { Location: `/?id=${title}` });
-          response.end();
-        });
-      });
-    });
+    renderUpdateProcessPage(request, response);
   } else if (pathname === "/delete_process") {
-    var body = "";
-    request.on("data", function (data) {
-      body = body + data;
-    });
-    request.on("end", function () {
-      var post = qs.parse(body);
-      var id = post.id;
-      var filteredId = path.parse(id).base;
-      fs.unlink(`data/${filteredId}`, function (error) {
-        response.writeHead(302, { Location: `/` });
-        response.end();
-      });
-    });
+    renderDeleteProcessPage(request, response);
   } else {
     response.writeHead(404);
     response.end("Not found");
